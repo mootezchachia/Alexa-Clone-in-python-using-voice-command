4e7d3ee2f34136a1879329634a1f12b70981dc15
Mootez Chachia <72633400+mootezchachia@users.noreply.github.com>
2022-12-19 04:27:53 +0100

    Update a.py

--- a.py
+++ a.py
@@ -5,9 +5,12 @@ import datetime
 import wikipedia
 import pyjokes
 
-# Import additional libraries for new features
+# Import additional libraries for OpenAI API
 import requests
 import json
+
+# Define OpenAI API key
+openai_key = "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"
 
 # Set up SpeechRecognizer
 listener = sr.Recognizer()
@@ -34,32 +37,59 @@ def take_command():
         pass
     return command
 
+# Function to answer questions using OpenAI API
+def answer_question(question):
+    # Set up OpenAI API request
+    request_data = {
+        "prompt": question,
+        "length": 50
+    }
+    headers = {"Authorization": "Bearer " + openai_key}
+    response = requests.post("https://api.openai.com/v1/engines/davinci/completions", json=request_data, headers=headers)
+    response_data = response.json()
+    # Get answer from response data
+    answer = response_data["choices"][0]["text"]
+    # Return answer
+    return answer
+
 # Function to run the Alexa skill
-def run_alexa():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I%M %p')
-        talk('Current time is ' + time)
-    elif 'who is' in command:
-        person = command.replace('who the heck is', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'date' in command:
-        date = datetime.datetime.now().strftime('%B %d, %Y')
-        talk('Current date is ' + date)
-    elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
+def run_alexa(command):
+    # If command is a question, answer it
+    if 'who' in command or 'what' in command or 'when' in command or 'where' in command or 'why' in command or 'how' in command:
+        if 'why' in command:
+            command = command.replace('why', '')
+        answer = answer_question(command)
+        talk(answer)
+    # If command is not a question, respond normally
     else:
-        talk('please say the command again')
+        if 'play' in command:
+            song = command.replace('play', '')
+            talk('playing ' + song)
+            pywhatkit.playonyt(song)
+        elif 'time' in command:
+            time = datetime.datetime.now().strftime('%I%M %p')
+            talk('Current time is ' + time)
+        elif 'who is' in command:
+            person = command.replace('who is', '')
+            info = wikipedia.summary(person, 1)
+            print(info)
+            talk(info)
+        elif 'date' in command:
+            date = datetime.datetime.now().strftime('%B %d, %Y')
+            talk('Current date is ' + date)
+        elif 'are you single' in command:
+            talk('I am in a relationship with wifi')
+        elif 'joke' in command:
+            talk(pyjokes.get_joke())
+        else:
+            talk('please say the command again')
 
 # Run Alexa skill
 while True:
-    run_alexa()
+    command_type = input("Enter 't' for text command or 'v' for voice command: ")
+    if command_type == 'v':
+        run_alexa(take_command())
+    elif command_type == 't':
+        command = input("Enter your command: ")
+        run_alexa(command)
+
